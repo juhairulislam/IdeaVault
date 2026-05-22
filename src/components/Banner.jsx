c71233901505d9ceb8a50622999621b35e402799
juhairulislam <juhairulislam2018@gmail.com>
2026-05-22 11:54:38 +0600

    add banner image and create banner file

---
+++ src/components/Banner.jsx
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            <Image
+            
+            ></Image>
+        </div>
+    );
+};
+
+export default Banner;
