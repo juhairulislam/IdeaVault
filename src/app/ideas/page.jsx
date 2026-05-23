124076d4e6b3fdb3f18daa665d415e2b435aa2e5
juhairulislam <juhairulislam2018@gmail.com>
2026-05-23 16:33:13 +0600

    idea title props remove

--- src/app/ideas/page.jsx
+++ src/app/ideas/page.jsx
@@ -54,7 +54,7 @@ const IdeasPage = async () => {
                     /* Multi-Column Responsive Grid Core Layer */
                     <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
                         {ideas.map((idea) => (
-                            <IdeaCard key={idea._id || idea.title} idea={idea} />
+                            <IdeaCard key={idea._id} idea={idea} />
                         ))}
                     </div>
                 )}
