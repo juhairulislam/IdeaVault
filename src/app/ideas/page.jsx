727a86906fc810fe3e65ae2d10d408146c3b7f38
juhairulislam <juhairulislam2018@gmail.com>
2026-05-23 08:07:03 +0600

    get all ideas in frontend ideas page

--- src/app/ideas/page.jsx
+++ src/app/ideas/page.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 
-const IdeasPage = () => {
+const fetchIdeas = async() =>{
+
+    const res = await fetch(`${process.env.SERVER_URL}/ideas`) ;
+    const data = await res.json() ;
+    return data || []
+}
+
+const IdeasPage =async () => {
+    const ideas = await fetchIdeas() ;
+    console.log(ideas)
     return (
         <div>
             This is ideas page
