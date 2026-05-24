9038edfc4ba62e9b73fe56e5cfe36aa5d715aa95
juhairulislam <juhairulislam2018@gmail.com>
2026-05-24 10:45:31 +0600

    change some name of input feild

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -13,6 +13,7 @@ const SignupPage = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        const formData = new FormData(e.currentTarget)
     };
 
     return (
@@ -83,8 +84,8 @@ const SignupPage = () => {
                                 <input
                                     required
                                     type="text"
-                                    name="fullName"
-                                    placeholder="John Doe"
+                                    name="name"
+                                    placeholder="Enter Your Name"
                                     className="w-full px-3.5 py-2.5 bg-zinc-50/50 dark:bg-zinc-950/50 border border-zinc-200/80 dark:border-zinc-800 rounded-xl text-zinc-900 dark:text-zinc-100 placeholder-zinc-400 dark:placeholder-zinc-600 focus:outline-none focus:ring-2 focus:ring-emerald-500/10 focus:border-emerald-500 dark:focus:border-emerald-500 transition-all duration-200 text-sm"
                                 />
                             </div>
@@ -111,7 +112,7 @@ const SignupPage = () => {
                                 <input
                                     required
                                     type="url"
-                                    name="photoUrl"
+                                    name="photo"
                                     placeholder="https://images.unsplash.com/photo-example"
                                     className="w-full px-3.5 py-2.5 bg-zinc-50/50 dark:bg-zinc-950/50 border border-zinc-200/80 dark:border-zinc-800 rounded-xl text-zinc-900 dark:text-zinc-100 placeholder-zinc-400 dark:placeholder-zinc-600 focus:outline-none focus:ring-2 focus:ring-emerald-500/10 focus:border-emerald-500 dark:focus:border-emerald-500 transition-all duration-200 text-sm"
                                 />
