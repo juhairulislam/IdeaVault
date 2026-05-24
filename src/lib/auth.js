bacdefdb5db6a15f92a32fb53081f8bab455a7c5
juhairulislam <juhairulislam2018@gmail.com>
2026-05-24 21:35:17 +0600

    set jwt in auth js

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,6 +1,8 @@
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { jwt } from "better-auth/plugins"
+
 
 const client = new MongoClient(process.env.MONGODB_URI);
 const db = client.db('ideavault');
@@ -12,5 +14,8 @@ export const auth = betterAuth({
   }),
   emailAndPassword: {    
         enabled: true
-    } 
+    }  ,
+    plugins: [
+        jwt(), 
+    ]
 });
