cb2e2c6b03149e45d594df0341d864e04be792a0
juhairulislam <juhairulislam2018@gmail.com>
2026-05-25 15:33:41 +0600

    make 4 routes privet by proxy

--- src/proxy.js
+++ src/proxy.js
@@ -1,13 +1,26 @@
+import { headers } from 'next/headers';
 import { NextResponse } from 'next/server'
+import { auth } from './lib/auth';
  
 // This function can be marked `async` if using `await` inside
-export function proxy(request) {
-  return NextResponse.redirect(new URL('/', request.url))
+export async function proxy(request) {
+  const session =   await auth.api.getSession({
+  
+    headers: await headers() // headers containing the user's session token
+});
+
+if(!session && !session?.user){
+
+    return NextResponse.redirect(new URL('/login', request.url))
+
+}
+
+
 }
  
 // Alternatively, you can use a default export:
 // export default function proxy(request) { ... }
  
 export const config = {
-  matcher: '/ideas/:id',
+  matcher: ['/ideas/:id' , '/add-idea' , '/my-interactions' , '/my-ideas'],
 }
