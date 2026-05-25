fbe564a5d0c890d8e5854a496431010dfddaca76
juhairulislam <juhairulislam2018@gmail.com>
2026-05-25 17:57:09 +0600

    get token in details page

--- src/app/ideas/[ideasId]/page.jsx
+++ src/app/ideas/[ideasId]/page.jsx
@@ -1,6 +1,17 @@
+import { auth } from '@/lib/auth';
+import { headers } from 'next/headers';
 import React from 'react';
 
-const IdeaDetailsPage = () => {
+const IdeaDetailsPage =async () => {
+
+      const token =   await auth.api.getToken({
+      
+        headers: await headers() 
+    });
+
+    console.log(token)
+    
+
     return (
         <div>
             This is idea details page 
