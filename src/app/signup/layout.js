bf314c96ba9facba456e6202a1da49565d2c2621
juhairulislam <juhairulislam2018@gmail.com>
2026-05-26 16:15:01 +0600

    add meta title in sign up page

---
+++ src/app/signup/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'IdeaVault | SignUp',
+  description: 'This is login page of IdeaVault',
+};
+
+export default function SimpleLayout({ children }) {
+  return (
+    <>
+      {children}
+    </>
+  );
+}
