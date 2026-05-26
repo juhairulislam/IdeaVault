865b96b5c9ff3095a2bf1993d4cb33c7b0fea468
juhairulislam <juhairulislam2018@gmail.com>
2026-05-26 13:20:14 +0600

    add in proxy or instead of and

--- src/proxy.js
+++ src/proxy.js
@@ -9,7 +9,7 @@ export async function proxy(request) {
     headers: await headers() // headers containing the user's session token
 });
 
-if(!session && !session?.user){
+if(!session || !session?.user){
 
     return NextResponse.redirect(new URL('/login', request.url))
 
