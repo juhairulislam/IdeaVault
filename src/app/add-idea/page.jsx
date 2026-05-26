1f6666a8129227b6a1f5b322b214507509784142
juhairulislam <juhairulislam2018@gmail.com>
2026-05-26 15:45:35 +0600

    add metadata in add idea page

--- src/app/add-idea/page.jsx
+++ src/app/add-idea/page.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
+export const metadata = {
+  title: "IdeaVault –Add Idea",
+  description: "This is Add Idea page of IdeaVault",
+};
+
 const AddIdeaPage = () => {
     return (
         <div>
