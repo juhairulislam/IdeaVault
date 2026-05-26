841d76d766e632b1a2e844282a8858f636b3e431
juhairulislam <juhairulislam2018@gmail.com>
2026-05-26 15:49:18 +0600

    add meta title in my interations page

--- src/app/my-interactions/page.jsx
+++ src/app/my-interactions/page.jsx
@@ -1,6 +1,13 @@
 import Link from 'next/link';
 import React from 'react';
 
+
+
+export const metadata = {
+  title: "IdeaVault –Add Interactions",
+  description: "This is Add Interactions page of IdeaVault",
+};
+
 const MyInteractionsPage = () => {
     return (
         <div>
